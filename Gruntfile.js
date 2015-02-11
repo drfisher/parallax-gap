4a06b30ced44173206b762b1d60ae4f323f34086
Pavel Rybin <drfisherozersk@gmail.com>
2015-02-11 22:13:07 +0300

    Added some new grunt tasks and a few functions in the ParallaxGap.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,68 @@
 module.exports = function (grunt) {
-	require('time-grunt')(grunt);
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-clean');
+  require('time-grunt')(grunt);
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		watch: {
-			scripts: {
-				files: '',
-				tasks: ['uglify'],
-				opptions: {
-					debounceDelay: 1000
-				}
-			}
-		},
+    watch: {
+      scripts: {
+        files: '',
+        tasks: ['uglify'],
+        opptions: {
+          debounceDelay: 1000
+        }
+      }
+    },
 
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> - <%= pkg.version %> */',
-				preserveComments: 'some'
-			},
-			all: {
-				files: {
-					'dist/ParallaxGap.min.js': [
-						'src/polyfills/*.js',
-						'src/ParallaxGap.js'
-					]
-				}
-			}
-		},
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - <%= pkg.version %> */',
+        preserveComments: 'some'
+      },
+      all: {
+        files: {
+          'dist/ParallaxGap.min.js': [
+            'src/polyfills/*.js',
+            'src/ParallaxGap.js'
+          ]
+        }
+      }
+    },
 
-		clean: ['dist']
-	});
-	grunt.registerTask('default', ['clean', 'uglify']);
+    jshint: {
+      options: {
+        nonew: true,
+        jquery: true,
+        validthis: true,
+        curly: true,
+        unused: true,
+        noarg: true,
+        loopfunc: true,
+        forin: true,
+        noempty: true,
+        undef: true,
+        bitwise: true,
+        browser: true,
+        devel: true,
+        predef: ['module', 'require']
+      },
+      all: ['Gruntfile.js', 'src/*.js']
+    },
+
+    jscs: {
+      src: ['Gruntfile.js', 'src/*.js'],
+      options: {
+        config: '.jscs.json'
+      }
+    },
+
+    clean: ['dist']
+  });
+
+  grunt.registerTask('default', ['jshint', 'jscs', 'clean', 'uglify']);
 };
--- src/ParallaxGap.js
+++ src/ParallaxGap.js
@@ -2,77 +2,122 @@
  * Parallax gap tool
  */
 (function (window) {
-	'use strict';
+  'use strict';
 
-	/**
-	 * @property {string} cssSelectors.layer
-	 * @property {string} cssSelectors.baseLayer
-	 * @property {string} cssSelectors.scrollCont
-	 */
-	var defaultOptions = {
-		cssSelectors: {
-			layer: '',
-			baseLayer: '', // TODO: is this selector realy needed?
-			scrollCont: ''
-		}
-	};
+  /**
+   * @property {string} cssSelectors.layer  All parallax layers
+   * @property {string} cssSelectors.baseLayer  One or more main layers
+   * @property {string} cssSelectors.scrollCont
+   */
+  var defaultOptions = {
+    cssSelectors: {
+      layer: '',
+      baseLayer: '', // TODO: is this selector realy needed?
+      scrollCont: ''
+    }
+  };
 
-	/**
-	 * @constructor
-	 */
-	var ParallaxGap = function () {};
+  /**
+   * Standart initialization for modern browsers
+   */
+  // var initViaNativeScroll = function () {};
 
-	ParallaxGap.prototype.init = function () {};
+  /**
+   * Fallback for browsers that don't support perspective
+   */
+  // var initViaLayerProperties = function () {};
 
-	/*
-	 * Public interface
-	 */
-	if (!(ParallaxGap in window)) {
-		window.ParallaxGap = (function () {
-			var useParamsCache = false;
-			var paramsCache = null;
+  /**
+   * @constructor
+   * @param {HTMLElement} container
+   * @param {object} options
+   */
+  var ParallaxGap = function (container, options) {
+    this._elements = {
+      container: container
+    };
+    this._options = options;
+  };
 
-			/**
-			 * Merges default options with user's params
-			 * @param {object} [params]
-			 * @return {object}
-			 */
-			var merge = function (params) {
-				params = params || {};
-				// TODO: merge
-			};
+  ParallaxGap.prototype.init = function () {
+    // TODO: find all elements
+    // TODO: add event listeners for custom and standart events
+  };
 
-			/**
-			 * Inits one container
-			 * @param {HTMLElement} container
-			 * @param {object} [params]
-			 */
-			var initContainer = function (container, params) {
-				if (useParamsCache && !paramsCache) {
-					paramsCache = merge(params);
-				}
-				// TODO: check for containter, it mast be a HTMLElement
-				return new ParallaxGap(container, useParamsCache ? paramsCache : merge(params));
-			};
+  /*
+   * Public interface
+   */
+  if (!(ParallaxGap in window)) {
+    window.ParallaxGap = (function () {
+      var useParamsCache = false;
+      var paramsCache = null;
 
-			return {
-				init: initContainer,
-				/**
-				 * Inits an array or array-like list of containers (it could be jQuery collection)
-				 * @param {*} containersList
-				 * @param {object} [params] Common params for all elements
-				 * @returns {*} list
-				 */
-				initAll: function (containersList, params) {
-					useParamsCache = true;
-					Array.prototype.forEach.call(containersList, function (currentContainer) {
-						initContainer(currentContainer, params);
-					});
-					useParamsCache = false;
-					paramsCache = null;
-					return containersList;
-				}
-			};
-		})();
-	}
+      /**
+       * Merges default options with user's params
+       * @param {object} [userParams]
+       * @param {object} [defaultParams]
+       * @return {object}
+       */
+      var mergeParams = function (userParams, defaultParams) {
+        userParams = userParams || {};
+        defaultParams = defaultParams || defaultOptions;
+        var result = {};
+        var currentValue, currentValueType;
+
+        for (var propName in defaultParams) {
+          if (defaultParams.hasOwnProperty(propName)) {
+            currentValue = userParams[propName] || defaultParams[propName];
+            currentValueType = getTypeOf(currentValue);
+            result[propName] = currentValueType === 'array' || currentValueType === 'object' ?
+              mergeParams(userParams[propName], defaultParams[propName])
+              : currentValue;
+          }
+        }
+        return result;
+      };
+
+      /**
+       * Inits one container
+       * @param {HTMLElement} container
+       * @param {object} [params]
+       */
+      var initContainer = function (container, params) {
+        if (useParamsCache && !paramsCache) {
+          paramsCache = mergeParams(params);
+        }
+        // TODO: check for containter, it mast be a HTMLElement
+        return new ParallaxGap(container, useParamsCache ? paramsCache : mergeParams(params));
+      };
+
+      return {
+        init: initContainer,
+        /**
+         * Inits an array or array-like list of containers (it could be jQuery collection)
+         * @param {*} containersList
+         * @param {object} [params] Common params for all elements
+         * @returns {*} list
+         */
+        initAll: function (containersList, params) {
+          useParamsCache = true;
+          Array.prototype.forEach.call(containersList, function (currentContainer) {
+            initContainer(currentContainer, params);
+          });
+          useParamsCache = false;
+          paramsCache = null;
+          return containersList;
+        }
+      };
+    })();
+  }
+
+  /**
+   * A slightly more informative "typeof"
+   * @param {*} testedValue
+   * @returns {string}
+   */
+  function getTypeOf (testedValue) {
+    return testedValue === null ? 'null'
+      : Array.isArray(testedValue) ? 'array'
+      : typeof testedValue;
+  }
 })(window);
